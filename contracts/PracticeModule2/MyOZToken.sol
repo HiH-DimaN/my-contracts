6e4b3f3fb44a29cf86adf5868632870bb8023574
HiH-DimaN <hih.diman@gmail.com>
2024-09-25 22:56:15 +0300

    delete prettierrc

--- contracts/PracticeModule2/MyOZToken.sol
+++ contracts/PracticeModule2/MyOZToken.sol
@@ -5,24 +5,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// Импортируем OpenZeppelin ERC20 и Ownable
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @title MyOZToken
- * @dev Контракт токена на основе ERC20 от OpenZeppelin.
- * @notice При деплое автоматически выпускает 1 000 000 токенов для владельца 
- * и сжигает 50% от общего количества.
+ * @title MyToken
+ * @dev Токен ERC20 с возможностью сжигания.
  */
-
 contract MyOZToken is ERC20, Ownable {
-    constructor() ERC20("My Custom Token", "MCT") Ownable() 
-        // Выпуск 1 000 000 токенов владельцу контракта
-        _mint(msg.sender, 1000000 * 10 ** decimals());
 
-        // Сжигание 50% от общего количества токенов
-        _burn(msg.sender, (1000000 * 10 ** decimals()) / 2);
-    
+    /**
+     * @dev Конструктор токена.
+     * Инициализирует токен с именем "My Token", символом "MYT"
+     * и создает 1 млн токенов для создателя контракта.
+     */
+    constructor() ERC20("My Token", "MYT") { 
+        _mint(msg.sender, 1_000_000 * 10**decimals()); // Минтим 1 млн токенов
+    }
+
+    /**
+     * @dev Сжигает 500 тыс. токенов, принадлежащих владельцу.
+     *
+     * Доступно только для владельца контракта.
+     */
+    function burnTokens() public onlyOwner {
+        _burn(msg.sender, 500_000 * 10**decimals());
+    }
 }
-
