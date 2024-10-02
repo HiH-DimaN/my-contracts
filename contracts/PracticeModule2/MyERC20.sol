cd34faaf0fa90df523163e7de5999d237e5c3060
HiH-DimaN <hih.diman@gmail.com>
2024-10-02 15:29:50 +0300

    add in MyERC20

--- contracts/PracticeModule2/MyERC20.sol
+++ contracts/PracticeModule2/MyERC20.sol
@@ -7,3 +7,37 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/** 
+ * @title MyERC20 
+ * @dev Базовая реализация стандарта ERC20.
+ */
+abstract contract MyERC20 is IERC20, Ownable {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) private _balances;
+
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+/**
+ * @dev Конструктор.
+ * @param name_ Название токена.
+ * @param symbol_ Символ токена.
+ * @param decimals_ Количество десятичных знаков.
+ */
+constructor(string memory name_, string memory symbol_, uint8 decimals_) {
+    _name = _name;
+    _symbol = symbol_;
+    _decimals = decimals_;
+}
+
+
+
+
+
+
+
+}
+
