58dab04276f5882e269a676aa60abead0ed4ec6f
HiH-DimaN <hih.diman@gmail.com>
2024-10-03 19:07:00 +0300

    added code in VotingWithDelegation

--- contracts/PracticeMiddle/VotingWithDelegation.sol
+++ contracts/PracticeMiddle/VotingWithDelegation.sol
@@ -35,5 +35,77 @@ contract VotingWithDelegation {
      */
     mapping(address => Voter) public voters;
 
+    /* @notice Событие, которое срабатывает при создании нового предложения.
+     * @param proposalIndex Индекс созданного предложения.
+     * @param description Описание предложения.
+     */
+    event ProposalCreated(uint256 proposalIndex, string description);
+
+    /* @notice Событие, которое срабатывает при голосовании участника.
+     * @param voter Адрес участника, который проголосовал.
+     * @param proposalIndex Индекс предложения, за которое проголосовали.
+     */
+    event VoterCast(address voter, uint256 proposalIndex);
+
+    /* @notice Событие, которое срабатывает при делегировании голосов.
+     * @param delegator Адрес участника, который делегировал свои голоса.
+     * @param delegate Адрес участника, которому были делегированы голоса.
+     */
+    event VoteDelegated(address delegator, address delegate);
+
+    // --- Конструктор ---
+
+    /* @notice Конструктор контракта. Создает начальные предложения.
+     * @param _proposalDescriptions Массив описаний предложений.
+     */
+    constructor(string[] memory _proposalDescriptions) {
+        // Цикл для создания начальных предложений на основе переданного массива
+        for (uint256 i = 0; i <= _proposalDescriptions.length; i++) {
+            // Создание новой структуры Proposal и добавление ее в массив proposals
+            proposals.push(Proposal(_proposalDescriptions[i], 0));
+            // Эмитирование события ProposalCreated для логирования создания предложения
+            emit ProposalCreated(i, _proposalDescriptions[i]);
+        }
+    }
+
+    // --- Функции ---
+
+    /* @notice Функция для регистрации нового участника голосования.
+     * @param _voterAddress Адрес участника.
+     * @param _initialWeight Начальный вес голоса участника.
+     */
+    function registerVoter(address _voterAddress, uint256 _initialWeight) public {
+        // Проверка, что участник еще не зарегистрирован
+        require(voters[_voterAddress].weight == 0, "Voter already registered");
+        
+        // Установка начального веса голоса для нового участника
+        voters[_voterAddress].weight = _initialWeight;
+    }
+
+    /* @notice Функция для делегирования голосов другому участнику.
+     * @param _to Адрес участника, которому делегируются голоса.
+     */
+    function delegate(address _to) public {
+        // Проверка, что нельзя делегировать голоса самому себе
+        require(_to != msg.sender, "Cannot delegate to self");
+        
+        // Получение ссылки на структуру Voter для текущего участника (msg.sender)
+        Voter storage sender = voters[msg.sender];
+
+        // Проверка, что участник зарегистрирован (имеет ненулевой вес голоса)
+        require(sender.weight > 0, "Voter not registered");
+        // Проверка, что участник еще не голосовал
+        require(sender.voted == false, "Voter already voted");
+
+        
+
+
+
+    }
+
+
+
+
+
     
 }
