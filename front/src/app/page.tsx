48dc471b749a56a2777a63f7a43f8c366367642f
HiH-DimaN <hih.diman@gmail.com>
2024-10-18 00:39:00 +0300

    added code in page.tsx

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -12,6 +12,9 @@ import WaitingFromTransactionMessage from "@/components/WaitingFromTransactionMe
 import TransactionErrorMessage from "@/components/TransactionErrorMessage";
 import WaitingForTransactionMessage from "./components/WaitingForTransactionMessage";
 import { error } from "console";
+import { title } from "process";
+import { strict } from "assert";
+import { validateHeaderName } from "http";
 
 const HARDHAT_NETWORK_ID = "0x539";
 const MUSIC_SHOP_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
@@ -24,13 +27,64 @@ type CurrentConnectionProps = {
   signer: JsonRpcSigner | undefined;
 };
 
+type AlbumProps = {
+  index: ethers.BigNumberish;
+  uid: string;
+  title: string;
+  price: ethers.BigNumberish;
+  quantity: ethers.BigNumberish;
+};
+
 
 export default function Home() {
   const [networkError, setNetworkError] = useState<string>();
   const [txBeingSent, setTxBeingSent] = useState<string>();
   const [transactionError, setTransactionError] = useState<any>();
+  const [currentBalance, setCurrentBalance] = useState<string>();
+  const [albums, setAlbums] = useState<AlbumProps[]>([]);
+  const [isOwner, setIsOwner] = useState<boolean>(false);
   const [currentConnection, setCurrentConnection] = 
     useState<CurrentConnectionProps>();
+
+  useEffect(() => {
+    (async () => {
+      if (currentConnection?.provider && currentConnection.signer) {
+        setCurrentBalance(
+          (
+            await currentConnection.provider.getBalance(
+              currentConnection.signer.address,
+              await currentConnection.provider.getBlockNumber(),
+            )
+          ).toString()
+
+        );
+      }
+
+    })();
+  }, [currentConnection, txBeingSent]); 
+  
+  useEffect(() => {
+    (async () => {
+      if (currentConnection?.shop && currentConnection.signer) {
+        const newAlbums = (await currentConnection.shop.allAlbums()).map((album) => {
+          return {
+            index: album.index,
+            uid: album.uid,
+            title: album.title,
+            price: album.price,
+            quantity: album.quantity,           
+          }
+        });
+
+        setAlbums((albums) => [...albums, ...newAlbums]);
+
+        setIsOwner(
+          ethers.getAddress(await currentConnection.shop.owner()) ===
+            (await currentConnection.signer.getAddress())
+        );
+      } 
+    })();
+  }, [currentConnection]);
 
   const _connectWallet = async () => {
     if (window.ethereum === undefined) {
@@ -81,6 +135,9 @@ export default function Home() {
   };
 
   const _resetState = () => {
+    setCurrentBalance(undefined);
+    setIsOwner(false);
+    setAlbums([]);
     setNetworkError(undefined);
     setTransactionError(undefined);
     setTxBeingSent(undefined);
@@ -123,6 +180,114 @@ export default function Home() {
     return error.mesage
   };
 
+  const _handleAddAlbum = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!currentConnection?.shop) {
+      return false;
+    }
+
+    const shop = currentConnection.shop;
+    const formData = new FormData(event.currentTarget);
+
+    const title = formData.get("albumTitle")?.toString();
+    const price = formData.get("albumPrice")?.toString();
+    const quantity = formData.get("albumQty")?.toString();
+
+    if (title && price && quantity) {
+      const uid = ethers.solidityPackedKeccak256(["string"], [title]);
+
+      try {
+        const index = await shop.currentIndex();
+
+        const addTx = await shop.addAlbum(
+          uid,
+          title,
+          BigInt(price),
+          BigInt(quantity)
+        );
+
+        setTxBeingSent(addTx.hash);
+
+        await addTx.wait();
+
+        setAlbums((albums) => [
+          ...albums,
+          {
+            index,
+            uid,
+            title,
+            price,
+            quantity,
+          },
+        ]);
+      } catch (err) {
+        console.error(err);        
+      } finally {
+        setTxBeingSent(undefined);
+      }
+    }
+  };
+
+  const _handleBuyAlbum = async (
+    album: AlbumProps,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+
+    if (!currentConnection?.shop) {
+      return false;
+    }
+
+    try {
+      const buyTx = await currentConnection.shop.buy(album.index, { value: album.price });
+      setTxBeingSent(buyTx.hash);
+      await buyTx.wait();
+
+      setAlbums(
+        albums.map((a) => {
+          if (a.index === album.index) {
+            album.quantity = 
+              BigInt(album.quantity) - BigInt(1);
+            return album;
+          } else {
+            return a;
+          }
+        })          
+      );
+    } catch (err) {
+      console.error(err);
+
+      setTransactionError(err);
+    } finally {
+      setTxBeingSent(undefined)
+    }
+  }
+
+  const availableAlbums = () => {
+    const albumsList = albums.map((album) => {
+      return (
+        <li key={album.uid}>
+          <>
+            {album.title} (#{album.index.toString()}<br />
+            Price: {album.price.toString()}
+            <br />
+            Qty: {album.quantity.toString()}
+            <br />
+            {BigInt(album.quantity) > BigInt(0) && (
+              <button onClick={(e) => _handleBuyAlbum(album, e)}>
+                Buy 1 copy
+              </button>
+            )}
+          </>
+        </li>
+      );
+    });
+
+    return albumsList;
+
+  };
+
   return (
     <main>
       {!currentConnection?.signer&& (
@@ -144,6 +309,35 @@ export default function Home() {
           message={_getRpsErrorMessage(transactionError)}
           dismiss={_dismissTransactionError}
         />
+      )}
+
+      {currentBalance && (
+        <p>Your balance: {ethers.formatEther(currentBalance)} ETH</p>
+      )}
+
+      {albums.length > 0 && <ul>{availableAlbums()}</ul>}
+
+      {isOwner && !txBeingSent && (
+        <form onSubmit={_handleAddAlbum}>
+          <h2>Add album</h2>
+
+          <label>
+            Title:
+            <input type="text" name="albumTitle" />
+          </label>
+
+          <label>
+            Price:
+            <input type="text" name="albumPrice" />
+          </label>
+
+          <label>
+            Qty:
+            <input type="text" name="albumQty" />
+          </label>
+
+          <input type="submit" name="Add!" />
+        </form>
       )}
     </main>
   );
