6a93b6983a49bcb8fc0f5eff337d3db2cc22db2f
HiH-DimaN <hih.diman@gmail.com>
2024-10-17 13:12:59 +0300

    added code in page.tsx

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect, FormEvent } from "react";
-import { ethers } from "ethers";
+import { ethers, JsonRpcSigner } from "ethers";
 import { MusicShop_factory } from "@/typechain";
 import { MusicShop } from "@/typechain";
 import { BrowserProvider } from "ethers";
@@ -10,108 +10,141 @@ import Image from "next/image";
 import ConnectWallet from "@/components/ConnectWallet";
 import WaitingFromTransactionMessage from "@/components/WaitingFromTransactionMessaget";
 import TransactionErrorMessage from "@/components/TransactionErrorMessage";
+import WaitingForTransactionMessage from "./components/WaitingForTransactionMessage";
+import { error } from "console";
 
 const HARDHAT_NETWORK_ID = "0x539";
 const MUSIC_SHOP_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 
 declare let window: any;
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+type CurrentConnectionProps = {
+  provider: BrowserProvider | undefined;
+  shop: MusicShop | undefined;
+  signer: JsonRpcSigner | undefined;
+};
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+export default function Home() {
+  const [networkError, setNetworkError] = useState<string>();
+  const [txBeingSent, setTxBeingSent] = useState<string>();
+  const [transactionError, setTransactionError] = useState<any>();
+  const [currentConnection, setCurrentConnection] = 
+    useState<CurrentConnectionProps>();
+
+  const _connectWallet = async () => {
+    if (window.ethereum === undefined) {
+      setNetworkError("Please install Metamask");
+
+      
+      return;
+    }
+
+    if (!(await _checkNetwork())) {
+      return;
+    }
+    
+    const [selectedAccount] = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    await _initialize(selectedAccount)
+
+    window.ethereum.on(
+      "accountChanged",
+      async ([newAccount]: [newAccount: string]) => {
+        if (newAccount === undefined) {
+          return _resetState();
+        }
+
+        await _initialize(newAccount);
+      }
+    );
+
+    window.ethereum.on("chainChanged", ([_networkId]: any) => {
+      _resetState;
+
+    });
+
+  };
+
+  const _initialize = async (slectedAccount: string) => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner(slectedAccount);
+
+    setCurrentConnection({
+      ...currentConnection,
+      provider,
+      signer,
+      shop: MusicShop_factory.connect(MUSIC_SHOP_ADDRESS, signer),
+    });
+  };
+
+  const _resetState = () => {
+    setNetworkError(undefined);
+    setTransactionError(undefined);
+    setTxBeingSent(undefined);
+    setCurrentConnection({
+      provider: undefined,
+      signer: undefined,
+      shop: undefined,
+    });
+  };
+
+  const _checkNetwork = async (): Promise<boolean> => {
+    const chosenChainID = await window.ethereum.request({
+      method: "eth_chainID",
+    });
+
+    if (chosenChainID === HARDHAT_NETWORK_ID) {
+      return true;
+    }
+
+    setNetworkError("Please connect to Hardhat network (localhost:8545)!");
+    
+    return false;
+
+  };
+
+  const _dismissNetworkError = () => {
+    setNetworkError(undefined);
+  };
+
+  const _dismissTransactionError = () => {
+    setTransactionError(undefined)
+  };
+
+  const _getRpsErrorMessage = (error: any): string => {
+    console.log(error);
+    if (error.data) {
+      return error.data.message
+    }
+
+    return error.mesage
+  };
+
+  return (
+    <main>
+      {!currentConnection?.signer&& (
+        <ConnectWallet
+          connectWallet={_connectWallet}
+          networkError={networkError}
+          dismiss={_dismissNetworkError}
+        />  
+      )}
+
+      {currentConnection?.signer&& (
+        <p>Your address: {currentConnection.signer.address}</p>
+      )}
+
+      {txBeingSent && <WaitingForTransactionMessage txHash={txBeingSent} />}
+
+      {transactionError && (
+        <TransactionErrorMessage
+          message={_getRpsErrorMessage(transactionError)}
+          dismiss={_dismissTransactionError}
+        />
+      )}
+    </main>
   );
 }
