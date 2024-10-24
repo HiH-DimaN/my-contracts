8da4d63a8cca8e7af837014006facecd0176bf2f
HiH-DimaN <hih.diman@gmail.com>
2024-10-24 22:49:18 +0300

    test folder Secuire

--- contracts/PracticeGuideDAO/Security/AuctionDenialOfService.sol
+++ contracts/PracticeGuideDAO/Security/AuctionDenialOfService.sol
@@ -14,22 +14,22 @@ contract AuctionDenialOfService {
         // pull необходимо использовать данный паттерн, когда участник сам запрашивает возврат средств
 
         // push небезопасный подход, т.к. возврат происходит с нашего смартконтракта и дает возможность для Denial of service
-        function refund() external {
-            for(uint i = 0; i < bidders.length; ++i) {
-                address currentBidder = biddrers[i];
+    function refund() external {
+        for(uint i = 0; i < bidders.length; ++i) {
+            address currentBidder = bidders[i];
 
-                uint refundAmount = bids[currentBidder];
+            uint refundAmount = bids[currentBidder];
 
-                bids(currentBidder) = 0;
+            bids[currentBidder] = 0;
 
-                if(refundAmount > 0) {
-                    (bool ok,) = currentBidder.call{value: refundAmount}();
-                    require(ok, "can`t send");
-                }
+            if(refundAmount > 0) {
+                (bool ok,) = currentBidder.call{value: refundAmount}("");
+                require(ok, "can`t send");
             }
         }
     }
 }
+
 
 contract Hack {
     AuctionDenialOfService toHack;
@@ -39,7 +39,7 @@ contract Hack {
     constructor(address _toHack) {
         require(msg.value == BID_AMOUNT);
 
-        toHack = AuctionDenialOfService(_tohack);
+        toHack = AuctionDenialOfService(_toHack);
         toHack.bid{value: msg.value}();
     }
 
