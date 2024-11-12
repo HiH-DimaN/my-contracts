ec6aa4e8c05ecbe0e18fb654897c05cf06d5d65e
HiH-DimaN <hih.diman@gmail.com>
2024-11-12 17:39:28 +0300

    added Proxy.sol

--- contracts/practice-GuideDAO/ERC2612/MyToken.sol
+++ contracts/practice-GuideDAO/ERC2612/MyToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "./ERC20.sol"; // Импорт базового контракта ERC20.
 import "./ERC20Permit.sol"; // Импорт контракта ERC20Permit для поддержки EIP-2612 (permit-функциональность).
