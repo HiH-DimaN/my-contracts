5b137776e0aacd03e63155430a3dfd10e497d074
HiH-DimaN <hih.diman@gmail.com>
2024-11-13 18:43:29 +0300

    added ERC1155.sol, IERC1155.sol, IERC1155MetadataURI.sol, IERC1155Receiver.sol

---
+++ contracts/practice-GuideDAO/ERC1155/IERC1155Receiver.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+interface IERC1155Receiver {
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint id,
+        uint amount,
+        bytes calldata data
+    ) external returns(bytes4);
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint[] calldata ids,
+        uint[] calldata amounts,
+        bytes calldata data
+    ) external returns(bytes4);
+}
